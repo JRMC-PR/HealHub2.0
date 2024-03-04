68cbdd93d3a99b8b16de36bac78b2c599a036dea
JRMC-PR <jesus.rafael.mendez.cruz>
2024-03-04 15:59:10 -0400

    Update forms

--- HealHub/accounts/forms.py
+++ HealHub/accounts/forms.py
@@ -5,17 +5,15 @@ from accounts.models import Accounts
 
 class SignupForm(forms.Form, UserCreationForm):
     USERNAME_FIELD = forms.CharField(max_length=40)
+    email = forms.EmailField()
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
     phone_number = forms.CharField(max_length=20)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
     is_doctor = forms.BooleanField(required=False, widget=forms.CheckboxInput) #Uses a checkbox to determine if the user is a doctor or not
 
     class Meta:
         model = Accounts
-        fields = ['USERNAME_FIELD', 'first_name', 'last_name', 'phone_number', 'email', 'password', 'confirm_password', 'is_doctor']
+        fields = ['USERNAME_FIELD', 'first_name', 'last_name', 'phone_number', 'email', 'password1', 'password2', 'is_doctor']
 
 class LoginForm(forms.Form):
     email = forms.EmailField()
--- HealHub/accounts/views.py
+++ HealHub/accounts/views.py
@@ -8,76 +8,51 @@ from .forms import SignupForm, LoginForm
 
 
 
-def home2(response):
-    if response.method == "POST":
-        signup_form = SignupForm(response.POST)
-        if signup_form.is_valid():
-            USERNAME_FIELD = signup_form.cleaned_data["username"]
-            first_name = signup_form.cleaned_data["first_name"]
-            last_name = signup_form.cleaned_data["last_name"]
-            email = signup_form.cleaned_data["email"]
-            password = signup_form.cleaned_data["password"]
-            confirm_password = signup_form.cleaned_data["confirm_password"]
-            is_doctor = signup_form.cleaned_data["is_doctor"]
-            phone_number = signup_form.cleaned_data["phone_number"]
-        if password == confirm_password:
-            user = Accounts(**signup_form.cleaned_data)
-            user.save()
-            return redirect("/dashboard", id=user.id)
+# def home2(response):
+#     if response.method == "POST":
+#         signup_form = SignupForm(response.POST)
+#         if signup_form.is_valid():
+#             USERNAME_FIELD = signup_form.cleaned_data["username"]
+#             first_name = signup_form.cleaned_data["first_name"]
+#             last_name = signup_form.cleaned_data["last_name"]
+#             email = signup_form.cleaned_data["email"]
+#             password = signup_form.cleaned_data["password"]
+#             confirm_password = signup_form.cleaned_data["confirm_password"]
+#             is_doctor = signup_form.cleaned_data["is_doctor"]
+#             phone_number = signup_form.cleaned_data["phone_number"]
+#         if password == confirm_password:
+#             user = Accounts(**signup_form.cleaned_data)
+#             user.save()
+#             return redirect("/dashboard", id=user.id)
 # Create your views here.
 def home(request):
     if request.method == "POST":
-      if 'login-form' in request.POST:
-          login_form = LoginForm(request.POST)
-          if login_form.is_valid():
-              email = login_form.cleaned_data.get('email')
-              password = login_form.cleaned_data.get('password')
-              user = authenticate(email=email, password=password)
-              if user is not None:
-                  login(request, user)
-                  return redirect("dashboard", id=user.id)
-          signup_form = SignupForm()
-      elif 'signup-form' in request.POST:
-          signup_form = SignupForm(request.POST)
-          if signup_form.is_valid():
-              USERNAME_FIELD = signup_form.cleaned_data["username"]
-              first_name = signup_form.cleaned_data["first_name"]
-              last_name = signup_form.cleaned_data["last_name"]
-              email = signup_form.cleaned_data["email"]
-              password = signup_form.cleaned_data["password"]
-              confirm_password = signup_form.cleaned_data["confirm_password"]
-              is_doctor = signup_form.cleaned_data["is_doctor"]
-              phone_number = signup_form.cleaned_data["phone_number"]
-          if password == confirm_password:
-              user = Accounts(**signup_form.cleaned_data)
-              user.save()
-              return redirect("dashboard", id=user.id)
-          login_form = LoginForm()
-    else:
-        login_form = LoginForm()
-        signup_form = SignupForm()
-    return render(request, "accounts/home.html", {"login_form": login_form, "signup_form": signup_form})
-    # if response.method == "POST":
-    #   form = SignupForm(response.POST)
-    #   if form.is_valid():
-    #       first_name = form.cleaned_data["first_name"]
-    #       last_name = form.cleaned_data["last_name"]
-    #       email = form.cleaned_data["email"]
-    #       password = form.cleaned_data["password"]
-    #       confirm_password = form.cleaned_data["confirm_password"]
-    #       is_doctor = form.cleaned_data["is_doctor"]
-    #       phone_number = form.cleaned_data["phone_number"]
-    #       if password == confirm_password:
-    #           user = Accounts(first_name=first_name, last_name=last_name, email=email, password=password, is_doctor=is_doctor, phone_number=phone_number)
-    #           user.save()
-    #           return redirect("dashboard", id=user.id)
-    #       else:
-    #           return HttpResponse('<h1>THE PASSWORDS DO NOT MATCH</h1>')
-    # else:
-    #     form = SignupForm()
-    # return render(response, "accounts/home.html", {"form": form})
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            USERNAME_FIELD = form.cleaned_data["USERNAME_FIELD"]
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            is_doctor = form.cleaned_data["is_doctor"]
+            phone_number = form.cleaned_data["phone_number"]
+            password = form.cleaned_data["password1"]
+            password2 = form.cleaned_data["password2"]
+            user = Accounts(USERNAME_FIELD=form.cleaned_data["USERNAME_FIELD"],
+                            first_name=form.cleaned_data["first_name"],
+                            last_name=form.cleaned_data["last_name"],
+                            email=form.cleaned_data["email"],
+                            is_doctor=form.cleaned_data["is_doctor"],
+                            phone_number=form.cleaned_data["phone_number"],
+                            password=form.cleaned_data["password1"],)
+            user.save()
 
-@login_required
+            return redirect("dashboard", id=user.id)
+    else:
+        signup_form = SignupForm()
+    return render(request, "accounts/home.html", {'form': form})
+
+
+
 def dashboard(response, id):
     user = Accounts.objects.get(id=id)
     first_name = user.first_name
