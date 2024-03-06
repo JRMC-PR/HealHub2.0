32966cae1935cb0c2d5569e97fd06c0aad0a09a5
JRMC-PR <jesus.rafael.mendez.cruz>
2024-03-06 14:33:49 -0400

    create new project

--- HealHub/accounts/models.py
+++ HealHub/accounts/models.py
@@ -5,33 +5,4 @@
     django user cmodels comes with these default fields
     """
 from django.db import models
-import uuid
-
-
-def unique_username():
-    """Generates a unique username"""
-    return f"username{uuid.uuid4().hex[:40]}"
-
-
-class Accounts(models.Model):
-    """This is the custom user model"""
-    #pulic fields for the user
-    USERNAME_FIELD = models.CharField(max_length=40, default=unique_username)
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200)
-    password = models.CharField(max_length=200, default="password")
-    phone_number = models.CharField(max_length=200)
-    contact_list = models.ManyToManyField("self", blank=True)
-    office_address = models.CharField(max_length=200, default="None")
-    is_doctor = models.BooleanField(default=False)
-    specialty = models.CharField(max_length=200, default="General")
-    created_at = models.DateTimeField(auto_now_add=True) #This sets the time of creation
-    updated_at = models.DateTimeField(auto_now=True) #This updates the time of modification
-
-    def __str__(self):
-        """Returns the user's full name, email, phone number and if they are a doctor or not"""
-        return f"{self.first_name} {self.last_name} {self.email} {self.phone_number} {self.is_doctor}"
-
-
 
--- HealHub/postgres.sql
+++ HealHub/postgres.sql
@@ -23,3 +23,30 @@ GRANT SELECT ON ALL TABLES IN SCHEMA public TO hh2_dev;
 -- you might need to execute them while connected to the specific database
 -- where you want these privileges to apply, especially for the GRANT commands
 -- on existing tables and setting default privileges.
+
+
+-- Create the 'hh_db' database (manually check if it exists first)
+-- DROP DATABASE IF EXISTS hh_db;
+CREATE DATABASE hh3_db;
+
+-- Create the 'hh_dev' user (manually check if it exists first)
+CREATE USER hh3_dev WITH PASSWORD 'HolbiTeam2024*';
+
+-- Grant all privileges on the 'hh_db' database to 'hh_dev'
+GRANT ALL PRIVILEGES ON DATABASE hh2_db TO hh3_dev;
+GRANT ALL PRIVILEGES ON SCHEMA public TO hh3_dev;
+
+-- Connect to the database where you want to apply the following settings
+\c hh2_db
+
+-- Grant SELECT on all future tables in the 'public' schema to 'hh_dev'
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO hh3_dev;
+
+-- This command grants SELECT on all existing tables in the 'public' schema to 'hh3_dev'
+-- Note: This needs to be executed in the 'hh_db' database context
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO hh3_dev;
+
+-- Reminder: To make sure the above commands work as expected,
+-- you might need to execute them while connected to the specific database
+-- where you want these privileges to apply, especially for the GRANT commands
+-- on existing tables and setting default privileges.
