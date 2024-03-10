55916d44d1090a37483702b1b3bf544402608da5
JRMC-PR <jesus.rafael.mendez.cruz>
2024-03-09 23:04:29 -0400

    Get appointment list and display in profile

--- HealHub2/accounts/views.py
+++ HealHub2/accounts/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db.models import Q
+from .models import Profile
+from appointments.models import Appointment
+from django.utils import timezone
+from datetime import timedelta
 
 
 # home view
@@ -49,6 +56,26 @@ def signup(request):
 
 
 
+
+@login_required
 def profile(request):
-    return render(request, "accounts/profile.html")
+    # Get the current user's profile to check if they are a doctor
+    user_profile, created = Profile.objects.get_or_create(user=request.user)
+    is_doctor = user_profile.doctor
+
+    # Calculate the date two weeks from now
+    two_weeks_ahead = timezone.now().date() + timedelta(weeks=2)
+
+    # Filter appointments up to two weeks ahead where the user is either the doctor or the patient
+    appointments = Appointment.objects.filter(
+        Q(doctor=request.user) | Q(patient=request.user),
+        date__lte=two_weeks_ahead
+    ).order_by('date', 'time')  # Ordering appointments by date and time
+
+    context = {
+        'appointments': appointments,
+        'is_doctor': is_doctor,
+    }
+
+    return render(request, 'accounts/profile.html', context)
 
