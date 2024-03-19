8ee8617a22d570f05a982ec5a658495b5866ad8e
JRMC-PR <jesus.rafael.mendez.cruz>
2024-03-19 12:13:29 -0400

    Adds members links

--- HealHub2/accounts/views.py
+++ HealHub2/accounts/views.py
@@ -72,11 +72,11 @@ def landing(request):
         {
             'name': 'Joshua Santiago Morales',
             'title': 'Fullstack (Backend)',
-            'description': 'Databse engenieer and backend developer ',
+            'description': 'In my role as Backend Engineer for HealHUb2.0 Web App, I designed and optimized the database to efficiently manage patient data and appointments. My efforts focused on ensuring data integrity, high performance, and scalability. Through optimized queries and security measures, I contributed to a seamless user experience and laid the groundwork for future enhancements, significantly impacting the app\'s success and reliability. ',
             'image': '',
             'social_media': {
-                'github': 'https://github.com/JCRoooD',
-                'linkedin': '#'
+                'github': 'https://github.com/Joshua7792',
+                'linkedin': 'https://www.linkedin.com/in/joshua-santiago00/'
             }
         },
     ]
